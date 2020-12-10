9c87d85c48fc758475ae43ca181a7f2dd2d50e98
tpreston96 <tpreston96@gmail.com>
2020-12-10 13:56:44 -0600

    FIXED ERROR

--- models/bread.js
+++ models/bread.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 const breadSchema = new Schema({
     name: {type: String, required: true},
     tasty: Boolean,
-    mouthfeel: {type: String, default: 'MOIST'}
+    mouthfeel: 
+    {type: String, default: "MOIST"}
 },{
     timestamps: true
 })
