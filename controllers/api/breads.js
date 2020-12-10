3c257f78868bfce83db9c1b41ec1692730e9ed6f
tpreston96 <tpreston96@gmail.com>
2020-12-10 13:41:19 -0600

    stubbed up functions

--- controllers/api/breads.js
+++ controllers/api/breads.js
@@ -1,1 +1,40 @@
 const Bread = require('../../models/bread')
+
+module.exports = {
+    index, 
+    show,
+    create,
+    update,
+    delete: deleteBread
+}
+
+function index(req, res) {
+    Bread.find({})
+    .then((breads) => {
+        res.status(200).json(breads)
+    })
+}
+function show(req, res) {
+    Bread.findById(req.params.id)
+    .then((bread)=> {
+        res.status(200).json(bread)
+    })
+}
+function create(req, res) {
+    Bread.create(req.body)
+    .then((bread)=> {
+        res.status(201).json(bread)
+    })
+}
+function update(req, res) {
+    Bread.findOneAndUpdate(req.params.id, req.body, {new: true})
+    .then((bread) => {
+        res.status(200).json(bread)
+    })
+}
+function deleteBread(req, res) {
+    Bread.findByIdAndDelete(req.params.id)
+    .then((bread)=> {
+        res.status(200).json(bread)
+    })
+}
